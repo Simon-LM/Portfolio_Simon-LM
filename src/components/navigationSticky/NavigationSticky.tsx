81a692df60f8e361ded1806d4746857000844822
Simon-LM <simon.lm.dev@bm.me>
2024-12-29 00:52:35 +0100

    Animations Menu Sticky

--- src/components/navigationSticky/NavigationSticky.tsx
+++ src/components/navigationSticky/NavigationSticky.tsx
@@ -7,6 +7,7 @@ import { RiMenu3Line } from "react-icons/ri";
 
 export default function NavigationSticky() {
 	const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+	const [isHovered, setIsHovered] = useState(false);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -19,10 +20,15 @@ export default function NavigationSticky() {
 	}, []);
 
 	return (
-		<div className={`navigation-sticky ${isHeaderVisible ? "with-text" : ""}`}>
+		<div
+			className={`navigation-sticky ${
+				isHeaderVisible || isHovered ? "show-text" : ""
+			}`}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}>
 			<button className="menu-button" aria-label="Menu principal">
 				<RiMenu3Line />
-				{isHeaderVisible && <span className="menu-text">Menu</span>}
+				<span className="menu-text">Menu</span>
 			</button>
 		</div>
 	);
