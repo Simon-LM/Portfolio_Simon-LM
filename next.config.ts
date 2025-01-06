7feb83dd0c9d76862cc31542844e1708257466a3
Simon-LM <simon.lm.dev@bm.me>
2025-01-06 20:24:07 +0100

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,25 +4,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	// output: "export",
-	// basePath: "/Portfolio_Simon-LM",
-	// images: {
-	// 	unoptimized: true,
-	// },
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/en",
-				permanent: true,
-			},
-			{
-				source: "/privacy-policy",
-				destination: "/en/privacy-policy",
-				permanent: true,
-			},
-		];
+	output: "export",
+	basePath: "/Portfolio_Simon-LM",
+	images: {
+		unoptimized: true,
 	},
+	trailingSlash: true,
+	// async redirects() {
+	// 	return [
+	// 		{
+	// 			source: "/",
+	// 			destination: "/en",
+	// 			permanent: true,
+	// 		},
+	// 		{
+	// 			source: "/privacy-policy",
+	// 			destination: "/en/privacy-policy",
+	// 			permanent: true,
+	// 		},
+	// 	];
+	// },
 };
 
 export default nextConfig;
